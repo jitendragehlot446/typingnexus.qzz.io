c513075b51bb8736b168c9f9942840d53486cade
User <factspatrika@gmail.com>
2026-06-11 02:53:47 +0530

    design: customize site with high-fidelity MonkeyType-inspired dark theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Typing Nexus Blogs';
+export const SITE_DESCRIPTION = 'The official blog for Typing Nexus - Level up your typing speed and accuracy.';
